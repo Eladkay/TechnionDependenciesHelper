f47a2e2dffe598c58575f1c3507b56e147a18d5a
Elad Kinsbruner <kinsbruner@campus.technion.ac.il>
2021-01-28 17:27:56 +0200

    Styling changes

--- index.php
+++ index.php
@@ -15,54 +15,63 @@
 
 </head>
 <body>
-<div style="text-align: center;"><h1> Technion Dependency Helper </h1></div>
-<h2><b> BETA </b>, report bugs! </h2>
-<p> Updated Jan 27th, 2021 </p>
-<a href="https://github.com/Eladkay/TechnionDependenciesHelper"> GitHub for issues and suggestions </a>
-<p> Enter here the course numbers you took, separated by spaces, and we will tell you what courses you can take! </p> <br>
-<form method="post">
-    <label for="courses">
-        Course numbers, separated by spaces:
-    </label>
-    <input type="text" name="courses" id="courses" value="<?php echo $_POST['courses']; ?>"/>
-    <br><br>
+<div class="container">
 
-    <label for="digits">
-        Optionally, the first three digits for course numbers to look through (for example, 236 for CS electives like
-        236363 Database Systems), defaults to 236:
-    </label>
-    <input type="text" name="digits" id="digits" value="<?php echo $_POST['digits']; ?>"/>
+    <div style="text-align: center;"><h1> Technion Dependency Helper </h1></div>
+    <h2><b> BETA </b>, report bugs! </h2>
+    <p> Updated Jan 27th, 2021 </p>
+    <a href="https://github.com/Eladkay/TechnionDependenciesHelper"> GitHub for issues and suggestions </a>
+    <p> Enter here the course numbers you took, separated by spaces, and we will tell you what courses you can take! </p> <br>
+    <form method="post">
+        <div class="input-group">
+            <label for="courses">
+                Course numbers, separated by spaces:&nbsp;&nbsp;&nbsp;
+            </label>
+            <input type="text" class="form-control" name="courses" id="courses" value="<?php echo $_POST['courses']; ?>"/>
+        </div>
+        <br>
+        <div class="input-group">
+            <label for="digits">
+                Optionally, filter only courses whose course numbers begin with the given three digits: &nbsp;&nbsp;&nbsp;
+            </label>
+            <input type="text" class="form-control" name="digits" id="digits" value="<?php echo $_POST['digits']; ?>"/>
+            <br>
+        </div>
+
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" name="filter" id="filter" value="yes" <?php if (isset($_POST["filter"])) echo "checked"; ?>>
+            <label class="form-check-label" for="filter">
+                Filter subjects with no dependencies
+            </label>
+        </div>
+
+        <!-- <input type="submit" name="bidusa"> <-- Bidusa Button </input> <br> <br> !-->
+
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" name="chem" id="chem" value="yes" <?php if (isset($_POST["chem"])) echo "checked"; ?>>
+            <label class="form-check-label" for="chem">
+                Include courses that require chemistry classification
+            </label>
+            <br>
+
+            <input class="form-check-input" type="checkbox" name="phys1" id="phys1" value="yes" <?php if (isset($_POST["phys1"])) echo "checked"; ?>>
+            <label class="form-check-label" for="phys1">
+                Include courses that require physics classification for mechanics
+            </label>
+            <br>
+
+            <input class="form-check-input" type="checkbox" name="phys2" id="phys2" value="yes" <?php if (isset($_POST["phys2"])) echo "checked"; ?>>
+            <label class="form-check-label" for="phys2">
+                Include courses that require physics classification for electricity
+            </label>
+        </div>
+
+        <br>
+
+        <div style="text-align: center;"><input type="submit"/></div>
+    </form>
     <br>
-
-    <input type="checkbox" name="filter" id="filter" value="yes" <?php if (isset($_POST["filter"])) echo "checked"; ?>>
-    <label for="filter">
-        Filter subjects with no dependencies
-    </label>
-    <br><br>
-
-    <!-- <input type="submit" name="bidusa"> <-- Bidusa Button </input> <br> <br> !-->
-
-    <input type="checkbox" name="chem" id="chem" value="yes" <?php if (isset($_POST["chem"])) echo "checked"; ?>>
-    <label for="chem">
-        Chemistry classification
-    </label>
-    <br>
-
-    <input type="checkbox" name="phys1" id="phys1" value="yes" <?php if (isset($_POST["phys1"])) echo "checked"; ?>>
-    <label for="phys1">
-        Physics classification for mechanics
-    </label>
-    <br>
-
-    <input type="checkbox" name="phys2" id="phys2" value="yes" <?php if (isset($_POST["phys2"])) echo "checked"; ?>>
-    <label for="phys2">
-        Physics classification for electricity
-    </label>
-    <br>
-
-    <input type="submit"/>
-</form>
-<br>
+</div>
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 'on');
@@ -141,7 +150,9 @@ if (isset($_POST["digits"]) && (strlen($_POST["digits"]) == 3 || $_POST["digits"
 echo "Courses you can take (with the correct tzmudim):<br>";
 $data = json_decode(file_get_contents("courses_202002.json"), true);
 if (!$data) echo "null!";
-echo "<table><tr><th>Course Number</th><th>Course Name</th><th>Requirements</th><th>Tzmudim</th></tr>";
+echo "<table class='table table-hover'>
+     <thead><tr><th>Course Number</th><th>Course Name</th><th>Requirements</th><th>Tzmudim</th></tr>
+     </thead><tbody>";
 
 $classifications = "";
 if (isset($_POST["chem"]) && $_POST["chem"] == "yes") $classifications .= " 123015 ";
@@ -162,7 +173,7 @@ foreach ($data as $course) {
         }
     }
 }
-echo "</table>";
+echo "</tbody></table>";
 ?>
 </body>
 </html>
