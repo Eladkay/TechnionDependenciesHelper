0a4d726521baa07b75423beac5a554f757f543fb
Elad Kinsbruner <kinsbruner@campus.technion.ac.il>
2021-09-20 00:01:59 +0300

    ???

--- index.php
+++ index.php
@@ -129,7 +129,7 @@
 
     //execute post
     $result = curl_exec($ch);
-    echo curl_error($ch);
+    echo curl_getinfo($ch);
     curl_close($ch);
 
     foreach (json_decode($result) as $course) {
